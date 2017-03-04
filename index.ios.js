5b9cb5a4994c6aa191a7514d7d8aad1c7b3938ea
Oladotun <oladotunopasina@gmail.com>
2017-03-04 07:56:33 -0500

    Added verification screen

--- index.ios.js
+++ index.ios.js
@@ -13,8 +13,14 @@ import {
   View,
   Image
 } from 'react-native';
-import Sinch from 'react-native-sinch-verification'
+import Sinch from 'react-native-sinch-verification';
+import Spinner from 'react-native-loading-spinner-overlay';
+import Form from 'react-native-form';
+import CountryPicker from 'react-native-country-picker-modal';
+
 var custom = '';
+// if you want to customize the country picker
+const countryPickerCustomStyles = {};
 export default class Oremi extends Component {
 
   componentDidMount() {
@@ -23,8 +29,12 @@ export default class Oremi extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      errorMessage: ''
+      enterCode: false,
+      spinner: false,
+      country: {
+        cca2: 'US',
+        callingCode: '1'
+      }
     };
 
     Sinch.init('333b66d3-b0da-4fe2-921a-7047947126fa');
@@ -45,16 +55,64 @@ export default class Oremi extends Component {
 });
   }
 
+  _renderCountryPicker = () => {
+
+    if (this.state.enterCode)
+      return (
+        <View />
+      );
+
+    return (
+      <CountryPicker
+        ref={'countryPicker'}
+        closeable
+        style={styles.countryPicker}
+        onChange={this._changeCountry}
+        cca2={this.state.country.cca2}
+        styles={countryPickerCustomStyles}
+        translation='eng'/>
+    );
+
+  }
+  _changeCountry = (country) => {
+    this.setState({ country });
+    // this.refs.form.refs.textInput.focus();
+  }
+
+  _renderCallingCode = () => {
+
+    if (this.state.enterCode)
+      return (
+        <View />
+      );
+
+    return (
+      <View style={styles.callingCodeView}>
+        <Text style={styles.callingCodeText}>+{this.state.country.callingCode}</Text>
+      </View>
+    );
+
+  }
+
   render() {
+    let headerText = `Whats your ${this.state.enterCode ? 'verification code' : 'phone number'}?`;
+    let buttonText = this.state.enterCode ? 'Verify confirmation code' : 'Send confirmation code';
+    let textStyle = this.state.enterCode ? {
+      height: 50,
+      textAlign: 'center',
+      fontSize: 40,
+      fontWeight: 'bold',
+      fontFamily: 'Courier'
+    } : {};
     return (
 
-      <View style={styles.container}>
 
-        <LinearGradient
+
+      <View style={styles.container}>
+      <LinearGradient
         colors={['#A1ADDB', '#D5D7D9','#F9AD67']}
         locations={[0,.75, 1.0]}
         style={styles.linearGradient}>
-
 
         <Text style={styles.welcome}>
           Welcome to React Native!
@@ -67,6 +125,7 @@ export default class Oremi extends Component {
           Cmd+D or shake for dev menu
         </Text>
         </LinearGradient>
+
         <Image
          style={styles.image}
          source={require('./img/oremiLogo.png')}
@@ -74,6 +133,17 @@ export default class Oremi extends Component {
        <Text style={styles.message}>
          OREMI
        </Text>
+       <Text style={styles.header}>{headerText}</Text>
+
+       <Form ref={'form'} style={styles.form}>
+
+         <View style={{ flexDirection: 'row' }}>
+         {this._renderCountryPicker()}
+         {this._renderCallingCode()}
+         </View>
+      </Form>
+
+
       </View>
 
     );
@@ -83,7 +153,6 @@ export default class Oremi extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'transparent'
     // backgroundColor: '#F9AD67'
@@ -95,7 +164,9 @@ const styles = StyleSheet.create({
     bottom: 0,
     right: 0,
     justifyContent: 'center'
-    // alignItems: 'center'
+  },countryPicker: {
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   image: {
     width: 150,
@@ -119,6 +190,13 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  header: {
+    textAlign: 'center',
+    marginTop: 0,
+    fontSize: 22,
+    margin: 20,
+    color: '#4A4A4A',
+  }
 });
 
 AppRegistry.registerComponent('Oremi', () => Oremi);
