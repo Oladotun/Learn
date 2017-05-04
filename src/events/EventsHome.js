2d30f4e4409f80ef988cb7563fa8e87b8f9294d0
Oladotun <oladotunopasina@gmail.com>
2017-05-03 21:37:55 -0400

    Fixed bug for create empty events

--- src/events/EventsHome.js
+++ src/events/EventsHome.js
@@ -31,7 +31,10 @@ export default class EventsHome extends Component {
       var self = this;
       let userRef =  database.ref('users/' + userUid + '/createdEvents/' );
           userRef.orderByChild("sortDate").startAt((new Date()).getTime()).on('value', function(snapshot) {
-            self.setState({createdEvents:snapshot.val()});
+            if (snapshot.val()){
+                self.setState({createdEvents:snapshot.val()});
+            }
+
 
           });
 
